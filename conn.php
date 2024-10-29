18508c9770e54ec54ac72c9aee1139395ca0e199
DESKTOP-GLIK0RL\edwar <edward.wee@outlook.com>
2024-10-30 04:20:36 +0800

    changedbpass

--- conn.php
+++ conn.php
@@ -1,12 +1,8 @@
 <?php
-/*
-	$servername = "imsenthusiast.com";
-	$username = "imsenthu_read";
-	$password = "Dki?_Au4o~Mq";
-	*/
+
 	
 	$servername = "127.0.0.1";
 	$username = "imsenthu_read";
-	$password ="WBH*8wbh" ;
+	$password ="WTF*8wtf" ;
 	$dbname="imsenthu_ms";
 	?>
