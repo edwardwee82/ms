fe93ecfc4adb86946f7bedf5fa4ffd66ecb1f1ad
DESKTOP-GLIK0RL\edwar <edward.wee@outlook.com>
2024-11-01 04:51:03 +0800

    c

--- conn.php
+++ conn.php
@@ -1,7 +1,8 @@
 <?php
 
 	
-	$servername = "127.0.0.1";
+	//$servername = "127.0.0.1";
+	$servername = "imsenthusiast.com";
 	$username = "imsenthu_read";
 	$password ="WTF*8wtf" ;
 	$dbname="imsenthu_ms";
