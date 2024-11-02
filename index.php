009c817649843de359b33d5335dae54f171aede4
DESKTOP-GLIK0RL\edwar <edward.wee@outlook.com>
2024-11-02 12:25:36 +0800

    x

--- index.php
+++ index.php
@@ -1,187 +1,41 @@
-<?php
-	//menu selections
-	$z="";
-	if(isset($_GET['menu']))
-	{
-		$menu= $_GET['menu'];
-		
-		if($menu=="System")
-		{
-			//$system= $_GET['sysID'];
-			
-				$z=getclause($_GET['sysID']);
-			
-		}
-		else
-		{
-			$z=getSystems();
-		}
-		
-	
-	}
-	else
-	{
-		$z=getSystems();
-	}
-	
-	
-?>
-
-<?php
-	//header details
-	$title="Integrated Management System Enthusiast";
-	//echo $servername;
-	
-?>
-
-<html>
+<!DOCTYPE html>
 <head>
-<title><?php echo $title; ?></title>
-<link rel="stylesheet" href="https://cdn.simplecss.org/simple.min.css">
+    <title>Update Clause Details</title>
+    <script src="https://cdn.tiny.cloud/1/tjwm8rfvtvnbrk7m1slkwdirjauctg8cffuleg4oqw3y4324/tinymce/7/tinymce.min.js" referrerpolicy="origin"></script>
+    <!-- <link rel="stylesheet" href="https://cdn.simplecss.org/simple.min.css"> -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 </head>
 <body>
-
-<h1><?php echo $title; ?></h1>
-
 <?php
-	//$x.= "test";
-	
-	
-	echo $z;
+	$menu=getmenu();
+	$output=getcontent();
 ?>
 
 
+<table width=100%>
+	<tr>
+		<td valign=top><?php echo $menu;  ?></td>
+		<td valign=top><?php echo $output; ?></td>
+	</tr>
+</table>
 </body>
 
-
-</html>
-
 <?php
-
-function getSystems()
+getmenu()
 {
-	include_once ".conn.php";
-	
-	// Create connection
-	$conn = new mysqli($servername, $username, $password, $dbname);
-	// Check connection
-	if ($conn->connect_error) {
-	  die("Connection failed: " . $conn->connect_error);
-	}
-	//$x ="Connected successfully <br>";
-	$x="";
-	
-	
-	
-	$sql = "SELECT * FROM `tSystems`;";
-	$result = $conn->query($sql);
-	if ($result->num_rows > 0) {
-	  // output data of each row
-	  $x.= "<table border=1>";
-/*	  $x.="<tr>
-		<th>sysID</th>
-		<th>System</th>
-		<th>Description</th>
-		</tr>";
-*/
-
-	  while($row = $result->fetch_assoc()) {
-		//$x.= "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
-		$x.= "<tr>";
-		//$x.= "<td><a href='index.php?menu=System&sysID=".$row["systemName"]."'>".$row["systemID"]."</a></td>";
-		$x.= "<td><a href='index.php?menu=System&sysID=".$row["systemName"]."'>".$row["systemName"]."</a></td>";
-		$x.= "<td>".$row["systemDescription"]."</td>";
-		$x.= "</tr>";
-	  }
-	  $x.= "</table>";
-	} else {
-	  $x.= "0 results";
-	}
-	$conn->close();
+	$x="<h5>Menu</h5>";
+	$x.="";
 	return $x;
-	
 }
 
-function getclause($sysid)
+getcontent()
 {
-	include_once ".conn.php";
-	
-	// Create connection
-	$conn = new mysqli($servername, $username, $password, $dbname);
-	// Check connection
-	if ($conn->connect_error) {
-	  die("Connection failed: " . $conn->connect_error);
-	}
-	//$x ="Connected successfully <br>";
-	$x="";
-	
-	
-	
-	$sql = "SELECT * FROM `tClauses` WHERE `systemName` LIKE '%$sysid%' ORDER BY `systemName` DESC, `clauseID` ASC;";
-	$result = $conn->query($sql);
-	if ($result->num_rows > 0) {
-	  // output data of each row
-	  
-	  
-	  //$x.=$result->num_rows." Rows For ".$sysid;
-	  $processName="";
-	  $x.="<h3>".$sysid."</h3>";
-		$cnt=1;
-	  while($row = $result->fetch_assoc()) {
-		  
-		if($row["processName"]!=$processName)
-		{	
-			$processName=$row["processName"];
-			if($cnt>1) 			{$x.="</table>\n";}
-			$x.="<h4>".$row["systemName"]." - ".$row["clauseName"]." ".$processName."</h4>\n";
-			$x.="\n<table border=0>\n";
-			$cnt+=1;
-			
-		}
-		else
-		{
-			$x.= "\n<tr>";
-			//$x.= "<td>".$row["clauseID"]."</td>";
-			$x.= "<td valign=top>".$row["clauseName"]."</td>\n";
-			$x.= "<td>".nl2br($row["clauseDescription"])."<br>\n".nl2br($row["clauseDetails"]);
-			if($row["ApplicabilityCriteria"]!=null)
-			{
-					$x.= "<br><b>Applicability</b><br>".nl2br($row["ApplicabilityCriteria"])."<br>\n";
-					
-			}
-			if($row["Intent"]!=null)
-			{
-					$x.= "<br><b>Intent</b><br>".nl2br($row["Intent"])."<br>\n";
-					
-			}
-			if($row["IntRef"]!=null)
-			{
-					$x.= "<br><b>Internal Ref</b><br>".nl2br($row["IntRef"])."<br>\n";
-					
-			}
-			if($row["ExtRef"]!=null)
-			{
-					$x.= "<br><b>Internal Ref</b><br>".nl2br($row["ExtRef"])."<br>\n";
-					
-			}
-			
-					
-					
-			
-			
-			$x.="</td>\n";
-			$x.= "</tr>\n";
-			$cnt+=1;
-		}
-	  }
-	  
-	} else {
-	  $x.= "0 results";
-	}
-	$conn->close();
-	return $x;
-	
-}
 
+	$x="<h5>Content</h5>";
+	$x.="<a href='viewms.php'>View Clauses</a>";
+	return $x;
+}
 
 ?>
--- viewms.php
+++ viewms.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <head>
-    <title>Update Clause Details</title>
+    <title>View Clause Details</title>
     <script src="https://cdn.tiny.cloud/1/tjwm8rfvtvnbrk7m1slkwdirjauctg8cffuleg4oqw3y4324/tinymce/7/tinymce.min.js" referrerpolicy="origin"></script>
     <!-- <link rel="stylesheet" href="https://cdn.simplecss.org/simple.min.css"> -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
